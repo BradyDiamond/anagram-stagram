27301d7a1b55867486b4821e45cca390b25e5020
Brady Diamond <brdydmnd@gmail.com>
2022-05-06 10:48:13 -0700

    troubleshoot second test

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,9 +1,17 @@
-class Word
-  def initilaize(word)
+class Word_check
+  def initilaize(word, word2)
     @word = word
+    @word2 = word2
   end
 
   def sort_word(word)
    return word.split('').sort().join('').to_s
-  end  
+   return word2.split('').sort().join('').to_s
+  end 
+  
+  def anagram_check(word, word2)
+    if word == word2
+    return  "this is an antagram"
+    end
+  end    
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -4,11 +4,14 @@ require('anagrams')
 
 describe("#sort") do
   it("will sort string to isolate characters") do
-    word = Word.new()
+    word = Word_check.new()
+    word2 = Word_check.new()
     expect(word.sort_word('hello')).to(eq('ehllo'))
+    expect(word2.sort_word('there')).to(eq('eehrt'))
   end
   it("will compare two sorted strings") do
-    word = Word.new()
-    word2 = Word.new()
-    expect(word.anagram_check('hello','hello')).to(eq(true))
+    word = Word_check.new()
+    word2 = Word_check.new()
+    expect(word.anagram_check('tea','eat')).to(eq("this is an antagram"))
+  end
 end
