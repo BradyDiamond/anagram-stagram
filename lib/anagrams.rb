94e36bc245cf752378aedb21818dcde6dbf9dac9
Brady Diamond <brdydmnd@gmail.com>
2022-05-06 11:40:34 -0700

    add third test

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,18 +1,20 @@
 class Word_check
-  def initilaize(word, word2)
+  def initilaize(str)
     @word = word
     @word2 = word2
   end
 
   def sort_word(word)
   return word.split('').sort().join('').to_s
+  return word2.split('').sort().join('').to_s
   end
 
   def anagram_check(word, word2)
-    word = word.sort_word()
-    word2 = word2.sort_word()
-    if word == word2
+    word_check = word.split('').sort().join('').to_s
+    word2_check = word2.split('').sort().join('').to_s
+    if word_check == word2_check
     return  "this is an antagram"
     end
+    return "this is not an antagram"
   end    
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -9,9 +9,14 @@ describe("#sort") do
     expect(word.sort_word('hello')).to(eq('ehllo'))
     expect(word2.sort_word('there')).to(eq('eehrt'))
   end
-  it("will compare two sorted strings") do
+  it("will compare two sorted strings and say if they are an antagram") do
     word = Word_check.new()
     word2 = Word_check.new()
-    expect(word.anagram_check('Schoolmaster','Theclassroom')).to(eq("this is an antagram"))
+    expect(word.anagram_check('pool','loop')).to(eq("this is an antagram"))
+  end
+  it("will compare two sorted strings and say if they are not an antagram") do
+    word = Word_check.new()
+    word2 = Word_check.new()
+    expect(word.anagram_check('pool','fool')).to(eq("this is not an antagram"))
   end
 end
