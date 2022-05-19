9a42c7740701948907887ab8e4bf667e954c613a
brady diamond <brady.diamond27@gmail.com>
2022-05-18 21:34:18 -0700

    add antigram functionality

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,3 +1,4 @@
+require ('pry')
 class WordCheck
   def initilaize(word, word2)
     @word = word
@@ -36,14 +37,14 @@ end
   
   def antigram_check(word, word2)
     anti_check = word.gsub!(/[^0-9A-Za-z]/, '')
-    anti_check = word.gsub(/\s+/, "").downcase.split('').sort().join('').delete(' ').to_s
+    anti_check = word.gsub(/\s+/, "").downcase.split('').sort()
     anti_check2 = word2.gsub!(/[^0-9A-Za-z]/, '')
-    anti2_check = word2.gsub(/\s+/, "").downcase.split('').sort().join('').delete(' ').to_s
-    big_string = anti_check + anti2_check
-    string_count = big_string.chars.uniq.count { |char| big_string.count(char) > 1}
-    if string_count < 1
+    anti_check2 = word2.gsub(/\s+/, "").downcase.split('').sort()
+    final_check = anti_check - anti_check2
+  
+    if final_check === anti_check
       return "no letters match: this is an antigram"
     end
-      return "there ARE matching letters: this is not an antigram"
-  end
+    return "there ARE matching letters: this is not an antigram" 
+  end 
 end
